4c5f70b5b719c9d68f5e2fabcbc2185dafd2bdce
Samoy <samoy_young@163.com>
2019-05-28 21:03:59 +0800

    将数据库修改成为复合主键以便于相同的表情包在不同分组下展示。

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ use fabiaoqing;
 # 创建类别表
 create table if not exists category
 (
-    objectId   varchar(16)  not null comment '主键' primary key,
+    objectId   varchar(16)  not null comment '类别id,主键' primary key,
     name       varchar(255) not null comment '类别名称',
     `order`    int          not null comment '次序',
     createTime datetime     not null default CURRENT_TIMESTAMP comment '创建时间',
@@ -14,22 +14,24 @@ create table if not exists category
 # 创建分组表
 create table if not exists `group`
 (
-    objectId   varchar(16)             not null comment '主键' primary key,
-    name       varchar(255) default '' not null comment '分组名称',
-    parentId   varchar(16)  default '' not null comment '类别id',
-    `order`    int                     not null comment '次序',
-    createTime datetime                not null default CURRENT_TIMESTAMP comment '创建时间',
-    updateTime datetime                not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间'
+    objectId   varchar(16) not null comment '分组id',
+    name       varchar(255)         default '' not null comment '分组名称',
+    parentId   varchar(16)          default '' not null comment '类别id（分组的父id）',
+    `order`    int         not null comment '次序',
+    createTime datetime    not null default CURRENT_TIMESTAMP comment '创建时间',
+    updateTime datetime    not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
+    primary key (objectId, parentId) comment '复合主键，当分组id和类别id均不相同时，才认为不是一条记录'
 );
 
 # 创建表情表
 create table if not exists emoticon
 (
-    objectId   varchar(16)             not null comment '主键' primary key,
-    name       varchar(255) default '' not null comment '表情包名称',
-    url        varchar(255)            not null comment '表情包地址',
-    parentId   varchar(16)  default '' not null comment '表情包的分组id',
-    `order`    int                     not null comment '次序',
-    createTime datetime                not null default CURRENT_TIMESTAMP comment '创建时间',
-    updateTime datetime                not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间'
+    objectId   varchar(16)  not null comment '表情包id',
+    name       varchar(255)          default '' not null comment '表情包名称',
+    url        varchar(255) not null comment '表情包地址',
+    parentId   varchar(16)           default '' not null comment '表情包的分组id',
+    `order`    int          not null comment '次序',
+    createTime datetime     not null default CURRENT_TIMESTAMP comment '创建时间',
+    updateTime datetime     not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP comment '更新时间',
+    primary key (objectId, parentId) comment '复合主键，当表情包id和分组id均不相同时，才认为不是一条记录'
 );
